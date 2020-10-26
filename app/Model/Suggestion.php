910f2c5b5d3be7ea3207fe2a12ebd5174a32cc44
abdulmuhabal <33183869+abdulmuhabal@users.noreply.github.com>
2020-10-26 17:59:01 +0800

    Addes suggestion endpoint

--- app/Model/Suggestion.php
+++ app/Model/Suggestion.php
@@ -3,8 +3,21 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Suggestion extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'message',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User','user_id');
+    }
 }
