bfee06563691a8843d8e7a5eb88d19db4793e198
abdulmuhabal <33183869+abdulmuhabal@users.noreply.github.com>
2020-10-27 17:40:04 +0800

    fixed log notification

--- app/Model/LogNotification.php
+++ app/Model/LogNotification.php
@@ -19,7 +19,7 @@ class LogNotification extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User','driver_id','id');
+        return $this->belongsTo('App\User','user_id','id');
     }
 
     public function userToNotify()
