88b2f8059a71e29a7a4597670083466fb641e0c2
abdulmuhabal <33183869+abdulmuhabal@users.noreply.github.com>
2020-11-03 19:53:18 +0800

    driver offer fixed

--- app/GraphQL/CustomQueries/DeliveryCustom.php
+++ app/GraphQL/CustomQueries/DeliveryCustom.php
@@ -84,7 +84,7 @@ class DeliveryCustom
     public function previousAsDriver($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         $driver_id = $args['driver_id'];
-        $driverOffer = DriverOffer::where("driver_id", $driver_id)->whereIn('status', ["ACCEPTED", "COMPLETED"]);
+        $driverOffers = DriverOffer::where("driver_id", $driver_id)->whereIn('status', ["ACCEPTED", "COMPLETED"]);
         if($driverOffers->get()->isEmpty())
             return $driverOffers;
         $driverOfferIds = $driverOffers->pluck('id');
