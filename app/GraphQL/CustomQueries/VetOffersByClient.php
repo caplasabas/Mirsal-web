06af92fe5022666ffc3741f8aa3fc76315e66ea2
abdulmuhabal <33183869+abdulmuhabal@users.noreply.github.com>
2020-11-24 12:40:14 +0800

    Fixed VetOffers

--- app/GraphQL/CustomQueries/VetOffersByClient.php
+++ app/GraphQL/CustomQueries/VetOffersByClient.php
@@ -21,4 +21,29 @@ class VetOffersByClient
         $vetOffers = VetOffer::where("status", $status)->whereIn('vet_request_id', $vetRequestIds);
         return $vetOffers;
     }
+
+    public function excludeWithVet($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+
+        $vetOfferQuery = VetOffer::query();
+        $vetRequestQuery = VetRequest::query();
+        if(isset($args['vet_request_id'])){
+            $vetRequestQuery = $vetRequestQuery->where("id",$args['vet_request_id']);
+        }
+        if(isset($args['created_with_vet'])){
+            $vetRequestQuery = $vetRequestQuery->where("created_with_vet",$args['created_with_vet']);
+        }
+        if(isset($args['created_with_vet']) || isset($args['vet_request_id'])){
+            $vetRequestIds = $vetRequestQuery->pluck("id");
+            $vetOfferQuery = $vetOfferQuery->whereIn("vet_request_id",$vetRequestIds);
+        }
+        if(isset($args['vet_id'])){
+            $vetOfferQuery = $vetOfferQuery->where("vet_id",$args['vet_id']);
+        }
+        if(isset($args['status'])){
+            $vetOfferQuery = $vetOfferQuery->where("status",$args['status']);
+        }
+
+        return $vetOfferQuery;
+    }
 }
